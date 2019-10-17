4e01fb857d67152b9150ac453acd7302524031ee
Шевелев Алексей <arbocdi@gmail.com>
2019-10-17 15:58:29 +0300

    добавил security & jpa
    (cherry picked from commit 52d499937e55345c631f714c77becc6c88f233dd)
    
    # Conflicts:
    #	chapter1/doc/chapter1.html
    #	chapter10/doc/jdbc.html
    #	chapter10/doc/jdbc.md
    #	chapter11/doc/chapter11.html
    #	chapter11/doc/chapter11.md
    #	chapter11/src/main/java/spitter/config/RootConfig.java
    #	chapter11/src/main/java/spitter/web/SpitterController.java
    #	chapter11/src/main/java/spitter/web/SpittleController.java
    #	chapter14/src/main/java/spitter/config/MethodSecurityConfig.java
    #	chapter16/doc/rest.html
    #	chapter16/doc/rest.md
    #	chapter16/src/main/java/spitter/config/SpitterWebappInitializer.java
    #	chapter16/src/main/java/spitter/web/SpilttleRestController.java
    #	chapter2/doc/chapter2.html
    #	chapter3/doc/chapter3.html
    #	chapter5/doc/chapter5.html
    #	chapter6/doc/chapter6.html
    #	chapter6/src/main/java/spitter/config/WebConfig.java
    #	chapter9/doc/chapter9.html
    #	chapter9/doc/chapter9.md
    #	chapter9/src/main/java/spitter/config/SecurityConfig.java
    #	readme.md
    #	settings.gradle

---
+++ chapter11/src/main/java/spitter/data/SpittleAutoRepositoryImpl.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spitter.data;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+/**
+ *
+ * @author root
+ */
+
+public class SpittleAutoRepositoryImpl implements SpittleRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public List<Spittle> findSpittles(int count) {
+        Query query =   em.createQuery("SELECT s FROM Spittle s");
+        query.setMaxResults(count);
+        List<Spittle> spittleList = query.getResultList();
+        return spittleList;
+    }
+
+    @Override
+    public Spittle findOne(long id) {
+        return em.find(Spittle.class, id);
+    }
+    
+}
